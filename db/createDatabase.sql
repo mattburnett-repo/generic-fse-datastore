a8f554952cf9674b839d168ac6cef96f73148ad6
Matt Burnett <mburnett@tutanota.de>
2022-03-30 11:17:21 +0200

    init commit

---
+++ db/createDatabase.sql
@@ -0,0 +1,75 @@
+
+-- create postgresql database as data store
+
+-- psql postgres -d feather_fullstack_code_challenge -f ./db/createDatabase.sql
+
+-- *** Create database in Postbird / some other Postgres tool of choice. ***
+--      trying to run -> psql postgres -d feather_fullstack_code_challenge -f -a ./db/createDatabase.sql
+--          results in error -> psql: error: connection to server on socket "/tmp/.s.PGSQL.5432" failed: FATAL:  database "feather_fullstack_code_challenge" does not exist
+--      trying to fix this with various permutations of command line args just results in more errors that just don't make sense
+
+BEGIN;
+
+DROP TABLE IF EXISTS policy CASCADE;
+DROP TABLE IF EXISTS customer CASCADE;
+DROP TABLE IF EXISTS provider CASCADE;
+DROP TABLE IF EXISTS insurance_type CASCADE;
+DROP TABLE IF EXISTS policy_status CASCADE;
+
+CREATE TABLE policy
+(
+    customer_id integer NOT NULL,
+    provider_id integer NOT NULL,
+    insurance_type_id integer NOT NULL,
+    status_id integer NOT NULL,
+    policy_number character varying(25),
+    start_date date,
+    end_date date,
+    created_at date DEFAULT CURRENT_DATE
+);
+
+CREATE TABLE customer
+(
+    id SERIAL PRIMARY KEY,
+    first_name character varying(25),
+    last_name character varying(25),
+    date_of_birth date
+);
+
+CREATE TABLE provider
+(
+    id integer UNIQUE NOT NULL,
+    description character varying(25) NOT NULL
+);
+
+CREATE TABLE insurance_type
+(
+    id integer UNIQUE NOT NULL,
+    description character varying(20) NOT NULL
+);
+
+CREATE TABLE policy_status
+(
+    id integer UNIQUE NOT NULL,
+    description character varying(20) NOT NULL
+);
+
+ALTER TABLE policy  
+    ADD CONSTRAINT policy_customer_id_fkey FOREIGN KEY (customer_id)
+    REFERENCES customer(id);
+
+ALTER TABLE policy  
+    ADD CONSTRAINT policy_provider_id_fkey FOREIGN KEY (provider_id)
+    REFERENCES provider(id);
+
+ALTER TABLE policy  
+    ADD CONSTRAINT policy_insurance_type_id_fkey FOREIGN KEY (insurance_type_id)
+    REFERENCES insurance_type(id);
+
+ALTER TABLE policy  
+    ADD CONSTRAINT policy_status_id_fkey FOREIGN KEY (status_id)
+    REFERENCES policy_status(id);
+
+END;
+
+SELECT '*** Run insertTestData.sql to add minimal test data ***' AS do_this_next;
---
+++ db/insertData.sql
@@ -0,0 +1,39 @@
+
+-- RUN CREATE SCRIPT USING FOLOWING LINE
+-- psql postgres -d feather_fullstack_code_challenge -f ./db/createDatabase.sql
+
+-- RUN THIS TO ADD MINIMAL TEST DATA (this file)
+-- psql postgres -d feather_fullstack_code_challenge -f ./db/insertData.sql
+
+-- POLICY STATUS 
+INSERT INTO policy_status(id, description) VALUES (1, 'Active');
+INSERT INTO policy_status(id, description) VALUES (2, 'Pending');
+INSERT INTO policy_status(id, description) VALUES (3, 'Cancelled');
+INSERT INTO policy_status(id, description) VALUES (4, 'Dropped out');
+
+-- INSURANCE TYPE
+INSERT INTO insurance_type(id, description) VALUES (1, 'Liability');
+INSERT INTO insurance_type(id, description) VALUES (2, 'Household');
+INSERT INTO insurance_type(id, description) VALUES (3, 'Health');
+
+-- PROVIDER
+INSERT INTO provider(id, description) VALUES (1, 'Allianz');
+INSERT INTO provider(id, description) VALUES (2, 'AXA');
+INSERT INTO provider(id, description) VALUES (3, 'ERGO Insurance Group');
+INSERT INTO provider(id, description) VALUES (4, 'DKV');
+
+-- CUSTOMER
+INSERT INTO customer(first_name, last_name, date_of_birth) VALUES('firstName 01', 'lastName 01', to_timestamp('1970-01-01', 'YYYY-MM-DD'));
+INSERT INTO customer(first_name, last_name, date_of_birth) VALUES('firstName 02', 'lastName 02', to_timestamp('1980-02-02', 'YYYY-MM-DD'));
+INSERT INTO customer(first_name, last_name, date_of_birth) VALUES('firstName 03', 'lastName 03', to_timestamp('1990-03-03', 'YYYY-MM-DD'));
+
+-- POLICY
+INSERT INTO policy(customer_id, provider_id, insurance_type_id, status_id, policy_number, start_date, end_date)
+VALUES (1, 1, 1, 1, 'ALLaaa111', to_timestamp('2000-01-01', 'YYYY-MM-DD'), to_timestamp('2001-01-01', 'YYYY-MM-DD'));
+INSERT INTO policy(customer_id, provider_id, insurance_type_id, status_id, policy_number, start_date, end_date)
+VALUES (2, 2, 2, 2, 'AXAbbb222', to_timestamp('2000-02-02', 'YYYY-MM-DD'), to_timestamp('2002-02-02', 'YYYY-MM-DD'));
+INSERT INTO policy(customer_id, provider_id, insurance_type_id, status_id, policy_number, start_date, end_date)
+VALUES (3, 3, 3, 3, 'ERGccc333', to_timestamp('2000-03-03', 'YYYY-MM-DD'), to_timestamp('2003-03-03', 'YYYY-MM-DD'));
+INSERT INTO policy(customer_id, provider_id, insurance_type_id, status_id, policy_number, start_date, end_date)
+VALUES (3, 2, 2, 4, 'ALLddd444', to_timestamp('2000-04-04', 'YYYY-MM-DD'), to_timestamp('2004-04-04', 'YYYY-MM-DD'));
+
